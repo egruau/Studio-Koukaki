977220e71e6f95a7ae619d52f1566cdb44850624
Elisa Gruau <lisale.kurosaki@gmail.com>
2023-07-29 01:34:38 +0200

    création rotation infinie fleurs

--- app/public/wp-content/themes/foce-child/functions.php
+++ app/public/wp-content/themes/foce-child/functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // wp_enqueue_style('animation', get_stylesheet_directory_uri(). './sass/animation.scss');
+    wp_enqueue_style('animation', get_stylesheet_directory_uri(). '/stylesheets/animation.css');
 }
 
 // Get customizer options form parent theme
